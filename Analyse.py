e15133d38c4d1f386cd600eb833401d9f3994a1d
drvinceknight <vincent.knight@gmail.com>
2013-05-27 15:58:03 +0100

    Added linear fit to regression line

--- Analyse.py
+++ Analyse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import matplotlib.pylab as plt
+from scipy import stats
 from csv import reader
 
 print "Reading data"
@@ -18,11 +19,17 @@ print "Drawing plot"
 plt.figure()
 x = [e[0] for e in basedata]
 y = [e[1] for e in basedata]
+gradient, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+line = [gradient * e + intercept for e in x]
 plt.scatter(x, y, label="Basic binary search")
+plt.plot(x, line, label="Fitted line: y=%.02fx+%.02f" % (gradient, intercept))
 plt.ylim(0, max(y))
 x = [e[0] for e in recursivedata]
 y = [e[1] for e in recursivedata]
+gradient, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+line = [gradient * e + intercept for e in x]
 plt.scatter(x, y, label="Recursive binary search", color="red")
+plt.plot(x, line, label="Fitted line: y=%.02fx+%.02f" % (gradient, intercept), color="red")
 plt.ylim(0, max(plt.ylim()[1], max(y)))
 plt.legend(loc=2)
 plt.savefig("Comparing_binary_search.pdf")
--- Compare.py
+++ Compare.py
@@ -5,6 +5,7 @@ Script to compare binary search written with recursive and non recursive code
 
 from __future__ import division
 import time
+import random
 from csv import writer
 
 
@@ -21,15 +22,12 @@ def timeit(code, trials=10):
     return mean(t)
 
 
-data = range(10)
-
-
 def binarysearch(target):
     """
     Code that carries out a binary search
     """
     first = 0
-    last = len(data) - 1
+    last = len(data)
     found = False
     while first <= last and not found:
         index = int((first + last) / 2)
@@ -59,18 +57,21 @@ def recursivebinarysearch(target, first, last):
 
 
 if __name__ == '__main__':
-    maxdatasize = 5000
+    numberofdatasets = 100
+    numberofpoints = 10
+    step = 2
     timings = []
     f = open("binarysearch.csv", "a")
     outfile = writer(f)
     recursivetimings = []
     rf = open("recursivebinarysearch.csv", "a")
     rec_outfile = writer(rf)
-    for k in range(1, maxdatasize + 1, 10):
+    for k in xrange(numberofpoints, numberofpoints + numberofdatasets + 1, step):
         print "Searching arrays of size %s" % k
         timings.append([k])
         data = range(k)
-        for target in range(k):
+        for i in xrange(numberofpoints):
+            target = random.choice(data)
             t = timeit('binarysearch(target)')
             rt = timeit('recursivebinarysearch(target, 0, len(data) - 1)')
             outfile.writerow([k, t])
