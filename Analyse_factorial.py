2237d89bd61285dc3895a4eaaedfca9cb114cc00
drvinceknight <vincent.knight@gmail.com>
2013-05-30 17:08:52 +0100

    two axes working

--- Analyse_factorial.py
+++ Analyse_factorial.py
@@ -19,10 +19,12 @@ print "Plotting scatter plots"
 x = []
 y1 = []
 y2 = []
+yc = []
 for row in data:
     x.append(row[0])
     y1.append(row[1])
     y2.append(row[2])
+    yc.append(row[1] / row[2])
 
 print "Interpolating"
 tck = interpolate.splrep(x, y1, s=1)
@@ -30,13 +32,22 @@ y1new = interpolate.splev(x, tck, der=0)
 tck = interpolate.splrep(x, y2, s=1)
 y2new = interpolate.splev(x, tck, der=0)
 
+print "Plotting"
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
 plt.scatter(x, y1, s=.2)
-plt.plot(x, y1new, label="Base", linewidth=2)
-plt.scatter(x, y2, color="red", s=.2)
-plt.plot(x, y2new, color="red", label="Recursive", linewidth=2)
-plt.legend(loc=2)
-plt.xlabel("$n!$")
-plt.ylabel("time (seconds)")
+ax1.plot(x, y1new, label="Iterative", linewidth=2)
+ax1.scatter(x, y2, color="red", s=.2)
+ax1.plot(x, y2new, color="red", label="Recursive", linewidth=2)
+ax1.legend(loc=2)
+ax1.set_xlabel("$n!$")
+ax1.set_ylabel("time (seconds)")
+
+ax2 = ax1.twinx()
+ax2.plot(x, yc, color="Black", label="Ratio")
+ax2.set_ylabel("Ratio of base to recursive time")
+
+plt.plot(x, [1 for e in x], color="Green", linestyle="dashed")
 plt.ylim(0, plt.ylim()[1])
 plt.xlim(0, plt.xlim()[1])
 plt.savefig("factorial.png")
